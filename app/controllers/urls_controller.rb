982f07e94c9151ff8637c5f5308aff2c4ba1660f
flemdizzle <fleminsh@gmail.com>
2014-10-10 16:21:47 -0400

    got routing set up
    It will successfully recognize routes based off the ID number

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,22 +1,28 @@
 class UrlsController < ApplicationController
-   layout 'main'
+  layout 'main'
   def new
-   @shortened_url = Url.new
+    @shortened_url = Url.new
   end
 
   def create
-   @shortened_url = Url.new(params[:url])
-      if @shortened_url.save
-         flash[:shortened_id] = @shortened_url.id
-         redirect_to new_url_url
-      else
-         render :action => "new"
-      end
+    @shortened_url = Url.new(url_params)
+    if @shortened_url.save
+      flash[:shortened_id] = @shortened_url.id
+      redirect_to new_url_url
+    else
+      render :action => "new"
+    end
   end
 
   def show
-   @shortened_url = Url.find(params[:id])
-   redirect_to @shortened_url.url
+    # shortened_url = Url.find_by_shortened_url(shortened_url: params[:shortened_url]).first
+    shortened_url = Url.find(params[:shortened_url])
+    redirect_to shortened_url.url
   end
-  
+
+  private
+
+  def url_params
+    params.require(:url).permit(:url)
+  end
 end
