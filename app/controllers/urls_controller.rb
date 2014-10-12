d5a95c3b1e87cad6c2c463ebcb7692ad5a132025
flemdizzle <fleminsh@gmail.com>
2014-10-12 01:13:44 -0400

    Things are working and I have no idea why
    It is succesfully forwarding from the main url/hex to the corresponding url and adding a hex value on creating of a new instance of url. Still need to the fix the copy and past link to provide and link to the right spot.

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,25 +1,14 @@
 class UrlsController < ApplicationController
   layout 'main'
 
-  def hex_random_generator
-    require 'SecureRandom'
-    hex = SecureRandom.urlsafe_base64(3)
-    # if hex is uniq
-    # return hex
-    # else
-    # hex_random_generator
-    # end
-  end
-
   def new
     @shortened_url = Url.new
-    @hex = hex_random_generator
   end
 
   def create
     @shortened_url = Url.new(url_params)
     if @shortened_url.save
-      flash[:shortened_id] = @shortened_url.id
+      flash[:shortened_id] = @shortened_url.hex
       redirect_to new_url_url
     else
       render :action => "new"
@@ -27,8 +16,7 @@ class UrlsController < ApplicationController
   end
 
   def show
-    # shortened_url = Url.find_by_shortened_url(shortened_url: params[:shortened_url]).first
-    shortened_url = Url.find(params[:shortened_url])
+    shortened_url = Url.find_by_hex(params[:shortened_url])
     redirect_to shortened_url.url
   end
 
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,16 +1,19 @@
 class Url < ActiveRecord::Base
    validates :url, presence: true
-   validates :hex, uniqueness: true, presence: true
+   # validates :hex, uniqueness: true, presence: true
+   before_save :hex_random_generator
 
+   # def initialize(attributes={})
+   #    super
+   #    @hex = hex_random_generator
+   # end
 
+# possibly add something to the model to check
+# the DB for the hex value. validates isn't working
   def hex_random_generator
     require 'SecureRandom'
     hex = SecureRandom.urlsafe_base64(3)
-    # if hex is uniq
-    # return hex
-    # else
-    # hex_random_generator
-    # end
+    self.hex = hex
   end
 
 end
